860c3f264b01325bfddeb6ac3c72198b803d64ac
PabloG <pawelgarbula@gmail.com>
2024-04-18 16:22:44 +0200

    correct code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,11 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe#vimeo-player');
 const player = new Player(iframe);
 
-const currentTime - localStorage.getItem("currentTime");
+const currentTime = localStorage.getItem('currentTime');
 if (currentTime) {
-    player.setCurrentTime(Number.parseFloat(currentTime));
+  player.setCurrentTime(Number.parseFloat(currentTime));
 }
 
-player.on('timeupdate', data) => {
-    localStorage.setItem("currentTime", data.duration);
-};
+player.on('timeupdate', data => {
+  localStorage.setItem('currentTime', data.duration);
+});
