4abdd16c7d196de921d775ccaaffb2224070e56f
PabloG <pawelgarbula@gmail.com>
2024-04-18 17:21:01 +0200

    code change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,13 @@
-import throttle from 'lodash.throttle';
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 
-const LOCAL_KEY = 'videoplayer-current-time';
-const THROTTLE_TIME = 1000;
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
 
-const player = new Vimeo('vimeo-player');
-player.setCurrentTime(getSavedVideoTime());
-
-player.on('timeupdate', throttle(onTimeupdate, THROTTLE_TIME));
-
-function onTimeupdate({ seconds }) {
-  saveVideoTimeToLocal(seconds);
+const currentTime = localStorage.getItem('currentTime');
+if (currentTime) {
+  player.setCurrentTime(Number.parseFloat(currentTime));
 }
 
-function saveVideoTimeToLocal(time) {
-  localStorage.setItem(LOCAL_KEY, String(time));
-}
-
-function getSavedVideoTime() {
-  return Number(localStorage.getItem(LOCAL_KEY)) || 0;
-}
+player.on('timeupdate', data => {
+  localStorage.setItem('currentTime', data.duration);
+});
