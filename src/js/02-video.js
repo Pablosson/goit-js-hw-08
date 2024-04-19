8ba35257aae08a44444a7dc5e479f8ad83cfdcd0
PabloG <pawelgarbula@gmail.com>
2024-04-19 15:07:05 +0200

    code change

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,23 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const player = new Player(document.getElementById('vimeo-player'));
 
-player.on(
-  'timeupdate',
-  _.throttle(() => {
-    player.getCurrentTime().then(time => {
-      localStorage.setItem('videoplayer-current-time', time);
-    });
-  }, 1000)
-);
+const saveCurrentTime = () => {
+  player.getCurrentTime().then(time => {
+    localStorage.setItem('videoplayer-current-time', time);
+  });
+};
 
-player.on('play', () => {
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+const resumePlayback = () => {
   const currentTime = localStorage.getItem('videoplayer-current-time');
   if (currentTime) {
     player.setCurrentTime(parseFloat(currentTime)).then(() => {
       player.play();
     });
   }
-});
+};
+
+player.ready().then(resumePlayback);
